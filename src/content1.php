c70857a4ebc5ce75ed5a149f69d46625abf24c72
Minna Barry <barrymin@onid.oregonstate.edu>
2015-05-02 15:33:15 -0700

    created login.php content1.php content2.php. wrote and tested login.php and content.php to make a user login and logout

---
+++ src/content1.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('didspllay_errors',1);
+
+session_start();
+if(session_status() == PHP_SESSION_ACTIVE){
+  if(isset($_POST["username"]) && $_POST["username"] !== ""){
+    if(!isset($_SESSION["username"])){
+	  $_SESSION["username"] = $_POST["username"];
+	  $_SESSION["visits"] = 0;
+	}
+  }
+     if(isset($_SESSION["username"]) && isset($_SESSION["visits"])){
+	    $_SESSION["visits"]++;
+		echo "Hello $_SESSION[username], you have visited this page $_SESSION[visits] times before. Click <a href=\"login.php?logout\">here</a> to logout. \n";
+	
+  }else{
+    echo 'please enter a username and login <a href="login.php">here</a>';
+  }
+}
+?>
---
+++ src/login.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('didspllay_errors',1);
+
+session_start();
+if(isset($_GET["logout"])){
+  $_SESSION = array();
+  session_destroy();
+}
+if(isset($_SESSION["username"])){
+  echo "<p> you are already logged in.</p>";
+} else {
+?>
+<form action="content1.php" method="post">
+  <label>username:</label>
+  <input type="text" name="username">
+  <button type="submit">login</button>
+</form>
+
+<?php
+}
+?>
